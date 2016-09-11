e5b93815b2df7e0d5ee997a882907963ee99220e
JM <jmjanzen@gmail.com>
2016-09-11 14:46:47 -0400

    Use array of cell objects to populate row object
    Rather than checking len meta data (which could simply be computed).
    
    This commit trades complexity in configuration, for simplicity in code.
    I'm not convinced this is the best approach...

--- doomguy.js
+++ doomguy.js
@@ -7,10 +7,9 @@ var drawing = [];
 rowProps.forEach(function (rows) {
   var row = new Row();
 
-  // TODO replace with for each cells
-  for (i = 0; i < rows.len; i++) {
-    row.addCell(rows.cells[0].char, rows.cells[0].colo);
-  }
+  rows.cells.forEach(function (cell) {
+    row.addCell(cell.char, cell.colo);
+  });
 
   drawing.push(row);
 });
