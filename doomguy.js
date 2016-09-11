d680cc8f2c3f76b08dbb9db82fff1d803e79b223
JM <jmjanzen@gmail.com>
2016-09-11 14:15:17 -0400

    Apply config from json directly rather than loading in memory first
    While here, add TODO note for when config contains proper cells arrays.

--- doomguy.js
+++ doomguy.js
@@ -5,20 +5,16 @@ var rowProps = require("./res/cells.json").rowProps;
 
 var drawing = [];
 var rows = [];
-rowProps.forEach(function (prop) {
-  rows.push(prop);
-});
-
-for (i = 0; i < rows.length; i++) {
-
+rowProps.forEach(function (rows) {
   var row = new Row();
 
-  for (j = 0; j < rows[i].len; j++) {
-    row.addCell(rows[i].cells[0].char, rows[i].cells[0].colo);
+  // TODO replace with for each cells
+  for (i = 0; i < rows.len; i++) {
+    row.addCell(rows.cells[0].char, rows.cells[0].colo);
   }
 
   drawing.push(row);
-}
+});
 
 drawing.forEach(function (row) {
   row.draw();
